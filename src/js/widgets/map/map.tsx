d4af5d2828b90fc637a0f8e8ddd214ce454dfcb0
danish.wajid <danish.wajid@fit.fraunhofer.de>
2024-05-14 12:39:48 +0200

    converted  widget to antdesign

--- src/js/widgets/map/map.tsx
+++ src/js/widgets/map/map.tsx
@@ -2,58 +2,35 @@ import React from "react";
 import { useTranslation } from "@opendash/core";
 import { createWidgetComponent } from "@opendash/plugin-monitoring";
 import { useDataService } from "@opendash/plugin-timeseries";
+import { Typography, Row, Col } from "antd";
 
 import { ConfigInterface } from "./types";
+import { Carousel } from "../../components/carousel";
 
-const styles = {
-  container: {
-    padding: "5%",
-    justifyContent: "center",
-  },
-  title: {
-    fontWeight: "bold",
-  },
-  dot: {
-    marginRight: "5px",
-  },
-  text: {
-    fontSize: "12px",
-  },
-  image: {
-    maxWidth: "100%",
-    display: "block",
-    marginBottom: "5%",
-    marginTop: "5%",
-  },
-  imageDescription: {
-    fontSize: "12px",
-    fontWeight: "bold",
-  },
-  propertyContainer: {
-    display: "flex",
-    flexDirection: "column",
-    marginTop: "5%",
-  },
-  propertyRow: {
-    display: "flex",
-    // justifyContent: "space-between",
-  },
+interface PropertyRowProps {
+  label: string;
+  value: string;
+}
 
-  propertyText: {
-    fontSize: "12px",
-    flex: 0.3,
-    fontWeight: "500",
-  },
-};
+const { Title, Text } = Typography;
+
+const PropertyRow: React.FC<PropertyRowProps> = ({ label, value }) => (
+  <Row>
+    <Text style={{ flex: 0.35, fontSize: "15px", fontWeight: "600" }}>
+      {label}:
+    </Text>
+    <Text style={{ flex: 0.65, fontSize: "15px" }}>{value}</Text>
+  </Row>
+);
 
 export default createWidgetComponent<ConfigInterface>(
   ({ config, ...context }) => {
     const t = useTranslation();
 
     context.setLoading(false);
-    const DataService = useDataService();
+    // const DataService = useDataService();
 
-    console.log("DataService", DataService);
+    // console.log("DataService", DataService);
 
     const properties = {
       name: "Plötzensee",
@@ -61,44 +38,50 @@ export default createWidgetComponent<ConfigInterface>(
       swimmingUsage: "Ja",
       district: "Mitte",
       circumference: "1645,102 m",
+      images: ["1", "2", "3"],
     };
 
     return (
-      <div style={styles.container}>
-        <h1 style={styles.title}>{properties.name}</h1>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <svg width="10" height="10" style={styles.dot}>
-            <circle cx="5" cy="5" r="5" fill="#55b169" />
-          </svg>
-          <text style={styles.text}>
-            {properties.name} I ggf. Zusatzinfos wie Zahl?
-          </text>
-        </div>
-        <img src={require("./map.png")} alt="Map" style={styles.image} />
-        <text style={styles.imageDescription}>
+      <div style={{ padding: "4%" }}>
+        <Title level={1} style={{ fontWeight: "bold", marginBottom: "2%" }}>
+          {properties?.name}
+        </Title>
+        <Row>
+          <Col
+            style={{
+              alignSelf: "center",
+            }}
+          >
+            <svg width="12" height="12">
+              <circle cx="6" cy="6" r="6" fill="#55b169" />
+            </svg>
+            <Text
+              strong
+              style={{
+                lineHeight: 0,
+                fontSize: "15px",
+                marginLeft: "5px",
+              }}
+            >
+              {properties?.name} I ggf. Zusatzinfos wie Zahl?
+            </Text>
+          </Col>
+        </Row>
+
+        <Carousel images={properties?.images} />
+        <Title level={5} style={{ fontWeight: "bold" }}>
           Bildbeschreibung zum obenstehenden Bild
-        </text>
-        <div style={styles.propertyContainer}>
-          <div style={styles.propertyRow}>
-            <text style={styles.propertyText}>{t("Name")}: </text>
-            <text style={styles.propertyText}>{properties.name}</text>
-          </div>
-          <div style={styles.propertyRow}>
-            <text style={styles.propertyText}>{t("Fläche")}: </text>
-            <text style={styles.propertyText}>{properties.area}</text>
-          </div>
-          <div style={styles.propertyRow}>
-            <text style={styles.propertyText}>{t("Badenutzung")}: </text>
-            <text style={styles.propertyText}>{properties.swimmingUsage}</text>
-          </div>
-          <div style={styles.propertyRow}>
-            <text style={styles.propertyText}>{t("Bezirk")}: </text>
-            <text style={styles.propertyText}>{properties.district}</text>
-          </div>
-          <div style={styles.propertyRow}>
-            <text style={styles.propertyText}>{t("Umfang")}: </text>
-            <text style={styles.propertyText}>{properties.circumference}</text>
-          </div>
+        </Title>
+
+        <div style={{ marginTop: "6%" }}>
+          <PropertyRow label={t("Name")} value={properties?.name} />
+          <PropertyRow label={t("Fläche")} value={properties?.area} />
+          <PropertyRow
+            label={t("Badenutzung")}
+            value={properties?.swimmingUsage}
+          />
+          <PropertyRow label={t("Bezirk")} value={properties?.district} />
+          <PropertyRow label={t("Umfang")} value={properties?.circumference} />
         </div>
       </div>
     );
